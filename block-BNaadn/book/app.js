87d5adfed795f72c69039dddb8ebad2fd5e9ba91
vivek <kumarvivek313@gmail.comgit>
2021-09-13 20:04:08 +0530

    A-03

--- block-BNaadn/book/app.js
+++ block-BNaadn/book/app.js
@@ -7,6 +7,7 @@ var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var bookRouter = require('./routes/book');
 
 mongoose.connect('mongodb://localhost/book', (err) => {
   console.log(err ? err : 'database connected');
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/books', bookRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- block-BNaadn/book/models/user.js
+++ block-BNaadn/book/models/user.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: String,
   country: String,
-  book: [{ type: Schema.Types.ObjectId, ref: 'Book' }],
+  books: [{ type: Schema.Types.ObjectId, ref: 'Book' }],
 });
 
 module.exports = mongoose.model('User', userSchema);
